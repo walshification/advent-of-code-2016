2e0f7c6aaa3f7b4d6180584af17d88d22419b653
Christopher Walsh <walshification@gmail.com>
2016-12-19 20:37:47 -0600

    ugly but it works

--- day-04/sector_sum.rb
+++ day-04/sector_sum.rb
@@ -4,4 +4,86 @@ class SectorSum
   def initialize(codes)
     @codes = codes
   end
+
+  def sum_codes
+    total = 0
+    @codes.each do |code|
+      if code.real?
+        total += code.sector_id
+      end
+    end
+    total
+  end
+end
+
+class Code
+  def initialize(code)
+    @code = code
+    @encrypted_name = nil
+    @sector_id = nil
+    @checksum = nil
+    @five_most_common_letters = nil
+  end
+
+  def encrypted_name
+    @encrypted_name ||= parse_name
+  end
+
+  def sector_id
+    @sector_id ||= parse_sector_id
+  end
+
+  def checksum
+    @check_sum ||= parse_checksum
+  end
+
+  def real?
+    validate_checksum
+  end
+
+  private
+
+  def parse_name
+    code_parts = @code.split('-')
+    @encrypted_name = code_parts[0, code_parts.length - 1].join
+  end
+
+  def parse_sector_id
+    @sector_id = /(\d{3})/.match(@code).captures.first.to_i
+  end
+
+  def parse_checksum
+    @checksum = /\[{1}(\w*)\]{1}/.match(@code).captures.first
+  end
+
+  def validate_checksum
+    (0..5).all? { |i| five_most_common_letters[i] == checksum[i] }
+  end
+
+  def five_most_common_letters
+    @five_most_common_letters ||= sort_by_value(encrypted_name.chars)
+  end
+
+  def sort_by_value(char_string)
+    sorted = {}
+    char_string.each { |letter| sorted[letter] = char_string.count(letter) }
+    # converts sorted to an array of array pairs from the hash
+    new_sorted = sorted.sort_by { |_, count| count }.reverse
+    # [["e", 2], ["t", 2], ["d", 1], ["o", 1], ["c", 1], ["s", 1]]
+    count_hash = {}
+    new_sorted.each do |letter_count_pair|
+      unless count_hash[letter_count_pair[1].to_s].nil?
+        count_hash[letter_count_pair[1].to_s] << letter_count_pair[0]
+      else
+        count_hash[letter_count_pair[1].to_s] = []
+        count_hash[letter_count_pair[1].to_s] << letter_count_pair[0]
+      end
+    end
+    count_hash.each do |_, letters|
+      letters.sort!
+    end
+    count_hash.map do |_, letters|
+      letters
+    end.join[0, 5]
+  end
 end
--- day-04/sector_sum_test.rb
+++ day-04/sector_sum_test.rb
@@ -10,10 +10,57 @@ ADVENT_INPUT = YAML.load_file('day-04/advent_input.yaml')
 class SectorSumTests < Minitest::Test
   def test_catalogs_a_list_of_room_codes
     codes = [
-      'bkwzkqsxq-tovvilokx-nozvyiwoxd-172[fstek]',
-      'wifilzof-wbiwifuny-yhachyylcha-526[qrazx]',
+      Code.new('bkwzkqsxq-tovvilokx-nozvyiwoxd-172[fstek]'),
+      Code.new('wifilzof-wbiwifuny-yhachyylcha-526[qrazx]'),
     ]
     sector_sum = SectorSum.new(codes)
     assert_equal(codes, sector_sum.codes)
+  end
+
+  def test_sums_sector_ids_of_real_rooms
+    codes = [
+      Code.new('test-code-100[etcdo]'),
+      Code.new('test-code-123[etcdo]'),
+    ]
+    sector_sum = SectorSum.new(codes)
+    assert_equal(223, sector_sum.sum_codes)
+  end
+
+  def test_skips_summing_secotr_ids_of_fake_rooms
+    sector_sum = SectorSum.new([Code.new('test-code-123[besty]')])
+    assert_equal(0, sector_sum.sum_codes)
+  end
+
+  def test_solves_the_puzzle
+    advent_codes = ADVENT_INPUT.map { |code| Code.new(code) }
+    sector_sum = SectorSum.new(advent_codes)
+    assert_equal(185371, sector_sum.sum_codes)
+  end
+end
+
+class CodeTests < Minitest::Test
+  def test_parses_the_code_for_its_encrypted_name
+    code = Code.new('test-code-123[besty]')
+    assert_equal('testcode', code.encrypted_name)
+  end
+
+  def test_parses_the_code_for_its_sector_id
+    code = Code.new('test-code-123[besty]')
+    assert_equal(123, code.sector_id)
+  end
+
+  def test_parses_the_code_for_its_sector_id
+    code = Code.new('test-code-123[besty]')
+    assert_equal('besty', code.checksum)
+  end
+
+  def test_knows_code_is_not_real_with_invalid_checksum
+    code = Code.new('test-code-123[besty]')
+    assert_equal(false, code.real?)
+  end
+
+  def test_knows_code_is_real_with_valid_checksum
+    code = Code.new('test-code-123[etcdo]')
+    assert_equal(true, code.real?)
   end
 end
