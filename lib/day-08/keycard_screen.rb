cdd1c2af5e0ff7d1bc38bc65316a315fb2b0d712
Christopher Walsh <walshification@gmail.com>
2016-12-30 12:33:24 -0600

    can rotate along y-axis now

--- lib/day-08/keycard_screen.rb
+++ lib/day-08/keycard_screen.rb
@@ -3,6 +3,8 @@ class KeycardScreen
 
   def initialize(screen=nil)
     @screen = screen || DEFAULT_SCREEN
+    @screen_height = @screen.length
+    @screen_width = @screen.first.length
   end
 
   def window
@@ -10,18 +12,34 @@ class KeycardScreen
   end
 
   def scan_keycard(command)
-    keyword, dimensions = command.split(' ')
-    if keyword == 'rect'
-      x_limit, y_limit = dimensions.split('x')
+    command_parts = command.split(' ')
+    if command_parts.first == 'rect'
+      x_limit, y_limit = command_parts[1].split('x')
       y = 0
       while y < y_limit.to_i
         x = 0
         while x < x_limit.to_i
-          @screen[y][x] = 'X'
+          @screen[y][x] = '#'
           x += 1
         end
         y += 1
       end
     end
+    if command_parts.first == 'rotate'
+      keyword, axis, column, _, limit = command_parts
+      dimension, value = column.split('=')
+      if dimension == 'x'
+        y = 0
+        new_value = nil
+        while y < @screen_height
+          new_value ||= @screen[(y - limit.to_i) % @screen_height][value.to_i]
+          current_value = @screen[y][value.to_i]
+          @screen[y][value.to_i] = new_value
+          new_value = current_value
+          y += 1
+        end
+      end
+    end
+    window
   end
 end
--- spec/day-08/keycard_screen_spec.rb
+++ spec/day-08/keycard_screen_spec.rb
@@ -18,15 +18,27 @@ RSpec.describe KeycardScreen do
   end
 
   describe '#scan_keycard' do
+    let(:screen) { KeycardScreen.new([
+        ['.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+      ]) }
+
     it 'turns pixels on when given a rect AxB command' do
-      screen = KeycardScreen.new([
-        ['.', '.', '.', '.'],
-        ['.', '.', '.', '.'],
-        ['.', '.', '.', '.'],
-        ['.', '.', '.', '.'],
-      ])
-      expected_screen = "XXX.\nXXX.\n....\n....\n"
+      expected_screen = "###....\n"\
+                        "###....\n"\
+                        ".......\n"
+
+      expect(screen.scan_keycard('rect 3x2')).to eql(expected_screen)
+    end
+
+    it 'rotates a specified column by a specified amount' do
+      expected_screen = "#.#....\n"\
+                        "###....\n"\
+                        ".#.....\n"
+
       screen.scan_keycard('rect 3x2')
+      screen.scan_keycard('rotate column x=1 by 1')
       expect(screen.window).to eql(expected_screen)
     end
   end
