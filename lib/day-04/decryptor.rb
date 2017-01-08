8fb110b20cfdb1429b71c70f6456e084c4ece3c2
Christopher Walsh <walshification@gmail.com>
2017-01-08 08:24:41 -0600

    aligns end keyword in decryptor

--- lib/day-04/decryptor.rb
+++ lib/day-04/decryptor.rb
@@ -15,5 +15,5 @@ class Decryptor
   def alphabet
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-   end
+  end
 end
